78231431a957e695bc7619ff278501be6139251c
cmarsh1 <cmarsh1@swarthmore.edu>
2018-06-04 13:52:54 -0400

    added javascript map to application output

--- src/java/edu/swarthmore/cs/cmarsh1/maproute/view/MapRouteApplication.java
+++ src/java/edu/swarthmore/cs/cmarsh1/maproute/view/MapRouteApplication.java
@@ -1,9 +1,12 @@
 package edu.swarthmore.cs.cmarsh1.maproute.view;
 
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.web.WebEngine;
@@ -14,7 +17,12 @@ public class MapRouteApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         //create toolbar buttons
+        HBox hBox = new HBox(10);
+        Button reset = new Button("Reset");
         Button mapRoute = new Button("Map Route");
+        Button createLooop = new Button("Create Loop");
+        TextField textField = new TextField();
+        hBox.getChildren().addAll(mapRoute, reset, createLooop, textField);
 
         //create the map display
         final WebView webView = new WebView();
@@ -25,11 +33,11 @@ public class MapRouteApplication extends Application {
         Text text  = new Text("Your route is soooo long!");
 
         BorderPane borderPane = new BorderPane();
-        borderPane.setTop(mapRoute);
+        borderPane.setTop(hBox);
         borderPane.setCenter(webView);
         borderPane.setBottom(text);
 
-        Scene scene  = new Scene(borderPane, 1000, 700, Color.web("#666970"));
+        Scene scene  = new Scene(borderPane, 1000, 750, Color.web("#666970"));
         stage.setScene(scene);
 
         stage.sizeToScene();
