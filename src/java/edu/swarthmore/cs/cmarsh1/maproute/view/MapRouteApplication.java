5b4bceee7cd6b6918c72ad1d58f2beafa5434c59
cmarsh1 <cmarsh1@swarthmore.edu>
2018-06-14 16:23:15 -0400

    changing to more js specific, added textbox over map

--- src/java/edu/swarthmore/cs/cmarsh1/maproute/view/MapRouteApplication.java
+++ src/java/edu/swarthmore/cs/cmarsh1/maproute/view/MapRouteApplication.java
@@ -2,6 +2,10 @@ package edu.swarthmore.cs.cmarsh1.maproute.view;
 
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
+import javafx.event.EventTarget;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -12,6 +16,9 @@ import javafx.scene.text.Text;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import org.w3c.dom.Element;
+import org.w3c.dom.events.Event;
+import org.w3c.dom.events.EventListener;
 import sun.plugin.javascript.JSObject;
 
 public class MapRouteApplication extends Application {
@@ -20,10 +27,9 @@ public class MapRouteApplication extends Application {
         //create toolbar buttons
         HBox hBox = new HBox(10);
         Button reset = new Button("Reset");
-        Button mapRoute = new Button("Map Route");
         Button createLooop = new Button("Create Loop");
         TextField textField = new TextField();
-        hBox.getChildren().addAll(mapRoute, reset, createLooop, textField);
+        hBox.getChildren().addAll(reset, createLooop, textField);
 
         //create the map display
         final WebView webView = new WebView();
@@ -31,13 +37,25 @@ public class MapRouteApplication extends Application {
         final WebEngine webEngine = webView.getEngine();
         webEngine.load(this.getClass().getResource("/edu/swarthmore/cs/cmarsh1/maproute/view/map.html").toString());
 
-        Text text = new Text("Run run run run run");
-
+//        Integer distance = 0;
+//        webEngine.getLoadWorker().stateProperty().addListener(new ChangeListener<Worker.State>() {
+//            @Override
+//            public void changed(ObservableValue<? extends Worker.State> observable, Worker.State oldValue, Worker.State newValue) {
+//                if(newValue == Worker.State.SUCCEEDED){
+//                    Element elt = webEngine.getDocument().getElementById("mapcanvas");
+//                    ((org.w3c.dom.events.EventTarget) elt).addEventListener("click", new EventListener() {
+//                        @Override
+//                        public void handleEvent(Event evt) {
+//                            System.out.println(webEngine.executeScript("getDistance()"));
+//                        }
+//                    }, false);
+//                }
+//            }
+//        });
 
         BorderPane borderPane = new BorderPane();
         borderPane.setTop(hBox);
         borderPane.setCenter(webView);
-        borderPane.setBottom(text);
 
         Scene scene  = new Scene(borderPane, 1000, 750, Color.web("#666970"));
         stage.setScene(scene);
